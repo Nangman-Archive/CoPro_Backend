b169669829a33d7265d32ff2cbfc02b2cf250d3b
giwoong <dev.choi0409@gmail.com>
2023-10-09 17:26:10 +0900

    first commit

---
+++ src/main/java/com/example/copro/CoProApplication.java
@@ -0,0 +1,13 @@
+package com.example.copro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoProApplication.class, args);
+    }
+
+}
