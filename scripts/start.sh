5d50d0156a716c795f8715597310ad02156fa577
seokhyeon <114850357+fr35wo@users.noreply.github.com>
2023-11-21 14:11:49 +0900

    Update start.sh
    Swagger 문서 생성

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,6 +13,10 @@ TIME_NOW=$(date +%c)
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
+# Swagger 문서 생성
+echo "$TIME_NOW > Swagger 문서 생성" >> $DEPLOY_LOG
+cd $PROJECT_ROOT && mvn swagger:generate
+
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
 nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
