458d9f5e1ea2036490597004eb05872e372d463f
giwoong <dev.choi0409@gmail.com>
2024-01-04 21:19:00 +0900

    refactor: 카드뷰 전체 멤버 찾기 memberSpecs 수정

--- src/main/java/com/example/copro/member/application/MemberService.java
+++ src/main/java/com/example/copro/member/application/MemberService.java
@@ -12,12 +12,14 @@ import com.example.copro.member.exception.InvalidMemberException;
 import com.example.copro.member.exception.NotFoundMemberException;
 import java.util.List;
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
 @Transactional(readOnly = true)
 public class MemberService {
     private final MemberRepository memberRepository;
