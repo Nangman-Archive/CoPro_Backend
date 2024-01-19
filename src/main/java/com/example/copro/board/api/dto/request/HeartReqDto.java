3e0d4e48c92633ee1ec8f18f8876ff9f1b6434ed
giwoong <dev.choi0409@gmail.com>
2024-01-19 15:54:52 +0900

    refactor: dto record로 수정

--- src/main/java/com/example/copro/board/api/dto/request/HeartReqDto.java
+++ src/main/java/com/example/copro/board/api/dto/request/HeartReqDto.java
@@ -1,16 +1,6 @@
 package com.example.copro.board.api.dto.request;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class HeartReqDto {
-    Long boardId;
-
-
-    public HeartReqDto(Long boardId){
-        this.boardId = boardId;
-    }
+public record HeartReqDto(
+        Long boardId
+) {
 }
