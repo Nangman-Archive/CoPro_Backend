aaf16703e93172ea09af5380f948d3a9b9af98c4
giwoong <dev.choi0409@gmail.com>
2024-01-23 19:11:47 +0900

    refactor: CommentResDto 이전 코드로 변경

--- src/main/java/com/example/copro/comment/domain/repository/CommentCustomRepository.java
+++ src/main/java/com/example/copro/comment/domain/repository/CommentCustomRepository.java
@@ -2,7 +2,6 @@ package com.example.copro.comment.domain.repository;
 
 import com.example.copro.comment.api.dto.response.CommentResDto;
 import com.example.copro.comment.domain.Comment;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +10,4 @@ public interface CommentCustomRepository {
     List<CommentResDto> findByBoardBoardId(Long boardId);
 
     Optional<Comment> findCommentByCommentIdWithParent(Long id);
-
-    int countByBoardBoardId(Long boardId);
 }
