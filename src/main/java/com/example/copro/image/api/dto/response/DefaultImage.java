332edd78a1b74226fd697ab53ff7dbebefd2cc23
giwoong <dev.choi0409@gmail.com>
2024-02-15 02:01:24 +0900

    feat: defaultImage 수정

--- src/main/java/com/example/copro/image/api/dto/response/DefaultImage.java
+++ src/main/java/com/example/copro/image/api/dto/response/DefaultImage.java
@@ -1,11 +1,9 @@
 package com.example.copro.image.api.dto.response;
 
-import org.springframework.beans.factory.annotation.Value;
-
 public enum DefaultImage {
    DEFAULT_IMAGE("https://d35sohbc9et6k7.cloudfront.net/35324cfc-e04a-4559-beab-f41a9c23d759_KakaoTalk_20231030_161509818.jpg");
 
-    final String imageUrl;
+    public final String imageUrl;
 
     DefaultImage(String imageUrl) {
         this.imageUrl = imageUrl;
