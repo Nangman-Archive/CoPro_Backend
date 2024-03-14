96b3ca2ad5fba7d3e00e0c35e7407f1bf57c5daa
giwoong <dev.choi0409@gmail.com>
2024-03-15 01:32:00 +0900

    refactor: 카드 뷰 로직 수정(회원 탈퇴, admin, nickName null 제외)

--- src/main/java/com/example/copro/member/application/MemberSpecs.java
+++ src/main/java/com/example/copro/member/application/MemberSpecs.java
@@ -32,12 +32,27 @@ public class MemberSpecs {
         return (root, query, criteriaBuilder) -> criteriaBuilder.notEqual(root.get("id"), member.getMemberId());
     }
 
-    public static Specification<Member> spec(String occupation, String language, int career, Member member) {
+    public static Specification<Member> isNotDeleted() {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.isFalse(root.get("isDelete"));
+    }
+
+    public static Specification<Member> isNicknameNotNull() {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.isNotNull(root.get("nickName"));
+    }
+
+    public static Specification<Member> excludeAdminEmail(String adminEmail) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.notEqual(root.get("email"), adminEmail);
+    }
+
+    public static Specification<Member> spec(String occupation, String language, int career, String adminEmail, Member member) {
         return Specification
                 .where(MemberSpecs.hasOccupation(occupation))
                 .and(MemberSpecs.hasLanguage(language))
                 .and(MemberSpecs.hasCareer(career))
-                .and(MemberSpecs.notCurrentMember(member));
+                .and(MemberSpecs.notCurrentMember(member))
+                .and(MemberSpecs.isNotDeleted())
+                .and(MemberSpecs.isNicknameNotNull())
+                .and(MemberSpecs.excludeAdminEmail(adminEmail));
     }
 }
 
