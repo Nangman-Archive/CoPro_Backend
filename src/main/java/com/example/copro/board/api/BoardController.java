bfb0d859b76ae758467e9a90b84bdbd2ba64ee7e
Hong, SeokHyeon <tyuiop929@gmail.com>
2024-08-06 23:36:54 +0900

    모니터링 툴 연동

--- src/main/java/com/example/copro/board/api/BoardController.java
+++ src/main/java/com/example/copro/board/api/BoardController.java
@@ -24,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
